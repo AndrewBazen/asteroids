8bf05af57a5c7ecbd0da73b7d156f24e945414b9
AndrewBazen <andrew.bazen@gmail.com>
2025-03-13 19:10:27 +0100

    minimum game function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 
 
@@ -6,12 +9,39 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+    
+    player = Player(x, y)
+    asteroid_field = AsteroidField()
+    
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updateable.update(dt)
         screen.fill("black")
+        
+        for d in drawable:
+            d.draw(screen)
+        
+        
+       
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
     
 
 if __name__ == "__main__":
